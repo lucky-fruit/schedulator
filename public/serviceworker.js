8bf2869a3278ab8cb326c92a0be60228e23a73fc
PinedaLD <ldmpineda@gmail.com>
2021-06-28 02:22:15 +0800

    Attempted Deployment

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -8,7 +8,7 @@ self.addEventListener('install', e => {
     e.waitUntil(
         cache.open(CACHE_NAME)
             .then((cache) => {
-                console.log('Opened cache')
+                console.log('Opened cache');
 
                 return cache.addAll(urlsToCache);
             })
@@ -17,10 +17,27 @@ self.addEventListener('install', e => {
 
 //Listen for requests
 self.addEventListener('fetch', e => {
-
+    e.respondWith(
+        caches.match(e.request)
+            .then(() => {
+                return fetch(e.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
 });
 
 // Activate the SW
 self.addEventListener('activate', e => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
 
+    e.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)){
+                    return caches.delete(cacheName)
+                }
+            })
+        ))
+    )
 });
