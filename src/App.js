452c40a7b2682f4ced5e8b3a4230fa33495b33c6
PinedaLD <ldmpineda@gmail.com>
2021-06-27 19:27:49 +0800

    glitchy Timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,119 @@
+import { Component } from 'react';
 import './App.css';
+import TimeDisplay from './components/TimeDisplay';
+import TypeSelect from './components/TypeSelect';
+import Controls from './components/Controls'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedType: props.types[0],
+      time: props.types[0].time,
+      interval: null,
+      running: false,
+    }
+  }
+
+  static defaultProps = {
+    types: [
+      { name: 'Work Time', time: 1500 },
+      { name: 'Short Rest', time: 300 },
+      { name: 'Long Rest', time: 900 }
+    ]
+  };
+
+
+  stopInterval = () => {
+    clearInterval(this.state.interval);
+    this.setState({ interval: null })
+  }
+
+  tick = () => {
+    if (this.state.time <= 1) {
+      this.stopInterval();
+      this.setState({ running: false });
+    }
+    console.log(this.state.time)
+    this.stopInterval();
+    this.setState(state => ({ time: state.time - 1 }))
+  };
+
+  startTimer = () => {
+    this.setState(state => ({
+      running: true,
+      interval: setInterval(this.tick, 1000),
+      time: state.time > 0 ? state.time : state.selectedType.time
+    }));
+  }
+
+  resetTimer = () => {
+    this.stopInterval();
+    this.setState(state => ({
+      time: state.selectedType.time,
+      running: false,
+    }))
+  }
+
+  pauseTimer = () => {
+    this.state.interval ? this.stopInterval() : this.startTimer();
+  }
+
+  getStatus = () => {
+    const { time, running, interval } = this.state;
+    if (time === 0) return 'Finished';
+    if (running && !interval) return 'Paused';
+    if (running) return 'Running';
+  }
+
+  getProgress = () => {
+    const current = this.state.time;
+    const total = this.state.selectedType.time;
+    return ((total - current) / total * 100);
+  }
+
+  changeType = type => {
+    this.resetTimer();
+    this.setState({ selectedType: type, time: type.time, running: false });
+  };
+
+
+  // tick (running the clock), stopInterval (clearing interval)
+  // startTimer, pauseTimer, resetTimer
+  // getStatus (finished, running, paused), getProgress (time elapsed/total time)
+  // changeType (toggle bet. work and rests)
+
+  render() {
+
+    const {time, selectedType} = this.state
+    const {types} = this.props
+
+    return (
+      <div className="App">
+        <TimeDisplay 
+          time={time}
+          status={this.getStatus()}
+          progress={this.getProgress()}
+        />
+
+        <Controls 
+          start={this.startTimer}
+          reset={this.resetTimer}
+          pause={this.pauseTimer}
+          status={this.getStatus()}
+        />
+        {/* <button onClick={this.startTimer}>Start Timer</button> */}
+
+
+        <TypeSelect 
+          types={types}
+          changeType={this.changeType}
+          selected={selectedType}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
